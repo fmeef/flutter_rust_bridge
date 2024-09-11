f225f09927b237652ae39871c05eb2bec32caadc
fmeef <gnu3ra@riseup.net>
2024-09-11 01:20:38 -0500

    port linter fix

--- tools/frb_internal/lib/src/makefile_dart/consts.dart
+++ tools/frb_internal/lib/src/makefile_dart/consts.dart
@@ -5,8 +5,6 @@ import 'package:flutter_rust_bridge_internal/src/utils/makefile_dart_infra.dart'
 const kRustPackagesAllowWeb = [
   'frb_rust',
   'frb_example/dart_minimal/rust',
-  'frb_example/pure_dart/rust',
-  'frb_example/pure_dart_pde/rust',
   'frb_example/dart_build_rs/rust',
   'frb_example/deliberate_bad/rust',
   'frb_example/integrate_third_party/rust',
@@ -16,6 +14,11 @@ const kRustPackagesAllowWeb = [
   'frb_example/rust_ui_counter',
   'frb_example/rust_ui_todo_list',
   'frb_example/gallery/rust',
+];
+
+const kRustPackagesTestFeature = [
+  'frb_example/pure_dart/rust',
+  'frb_example/pure_dart_pde/rust',
 ];
 
 const kRustPackagesDisallowWeb = [
