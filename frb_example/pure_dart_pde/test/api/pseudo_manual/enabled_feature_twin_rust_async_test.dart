0f92d5e86a6a3da93972d236bb9a7ba6d15b911f
fmeef <gnu3ra@riseup.net>
2024-09-11 01:50:58 -0500

    missing files

---
+++ frb_example/pure_dart_pde/test/api/pseudo_manual/enabled_feature_twin_rust_async_test.dart
@@ -0,0 +1,19 @@
+// NOTE: This file is mimicking how a human developer writes tests,
+// and is auto-generated from `enabled_feature_test.dart` by frb_internal
+// Please do not modify manually, but modify the origin and re-run frb_internal generator
+
+// AUTO-GENERATED FROM frb_example/pure_dart, DO NOT EDIT
+
+// FRB_INTERNAL_GENERATOR: {"addCode": "import 'package:frb_example_pure_dart_pde/src/rust/api/pseudo_manual/array_twin_rust_async.dart';"}
+
+import 'package:frb_example_pure_dart_pde/src/rust/api/pseudo_manual/misc_no_twin_example_a_twin_rust_async.dart';
+import 'package:frb_example_pure_dart_pde/src/rust/frb_generated.dart';
+import 'package:test/test.dart';
+
+Future<void> main({bool skipRustLibInit = false}) async {
+  if (!skipRustLibInit) await RustLib.init();
+
+  test('onlyTestFeature exists', () async {
+    expect(onlyTestFeature(), "test");
+  });
+}
